45cb15da38729e86b9d388727f790bad41be3bc3
amerigirl <93811176+amerigirl@users.noreply.github.com>
2024-01-05 20:22:53 +0000

    added move chars forward method

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));  
 
 console.log("1. Get the file extenstion. File example = www.google.com.  Answer = " + getFileExtension('www.google.com'))
+
+//2. Write a javascript program to replace every character in a given string with the character following it in the alphabet
+
+const moveCharsForward = (str) => str
+
+  .split('')
+  .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+  .join('');  
+
+console.log("\n" + "2. Move characters forward one char in the alphabet. Chars to move: abcd. Answer = " + moveCharsForward("abcd"));
