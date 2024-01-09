67d0d17ff657b9ea2f9d6afb7fc81d490eb91298
amerigirl <93811176+amerigirl@users.noreply.github.com>
2024-01-09 13:35:26 +0000

    quick morning overview of a couple of practice questions

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 //1. Write a javascript program to get the extension of a filename
 
-const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));  
+const getFileExtension = (str) => str.slice(str.lastIndexOf("."));
 
-console.log("1. Get the file extenstion. File example = www.google.com.  Answer = " + getFileExtension('www.google.com'))
-
-
-
-
+console.log(
+  "1. Get the file extenstion. File example = www.google.com.  Answer = " +
+    getFileExtension("www.google.com"),
+);
 
 //2. Write a javascript program to replace every character in a given string with the character following it in the alphabet
 
-const moveCharsForward = (str) => str
+const moveCharsForward = (str) =>
+  str
 
-  .split('')
-  .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
-  .join('');  
+    .split("") //remember split turns into an array--which gives access to array methods
+    .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join("");
 
-console.log("\n" + "2. Move characters forward one char in the alphabet. Chars to move: abcd. Answer = " + moveCharsForward("abcd"));
-
-
-
-
-
-
+console.log(
+  "\n" +
+    "2. Move characters forward one char in the alphabet. Chars to move: abcd. Answer = " +
+    moveCharsForward("abcd") + "."
+);
 
 //3. write a javascript program to get the current date
 
-const getDate = date => {
+const getDate = (date) => {
   //create a new date object
   date = new Date();
   const day = date.getDate(); //use the date object methods
@@ -36,4 +34,18 @@ const getDate = date => {
   return `${month}/${day}/${year}`; //return a template literal
 };
 
-console.log("\n" + '3. Return the current date. Answer = ' + getDate());
+console.log("\n" + "3. Return the current date. Answer = " + getDate() + ".");
+
+//4. Write a javascript program to create a new string adding "New!" in front of a given string.
+
+const addNew = (str) => {
+  "New! " + str;
+
+  if (str.indexOf("New!")) {
+    return str;
+  } else {
+    return "New! " + str;
+  }
+};
+
+console.log("\n" + "4. Add 'New' to the front of a string. Answer = " + addNew('New' + ' I love New York' + "!"));
